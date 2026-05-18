da93e03547726d6dc8eb313c5ee320ce054ccd27
Shaurya Rastogi <shaurya.2428cseaiml2@kiet.edu>
2026-05-18 19:49:48 +0530

    update backend url for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-
-  baseURL: "http://localhost:10000/api",
-
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json"
   }
-
 });
 
 export default API;
